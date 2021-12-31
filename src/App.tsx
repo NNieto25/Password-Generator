aec9290f257c2862cc84a0ac84d33e6f56bfde46
NNieto25 <00199019@uca.edu.sv>
2021-12-31 14:48:24 -0600

    Modified: App Component & Password Component styles Added the base styles for the main app component, color variables, and implementing password component on App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
+import Password from "./Components/Password/Password";
+
 function App() {
+  const [password, setPassword] = useState<string>("Ex4mPl3P4ssW0rd");
   return (
     <div className="App">
+      <Password text={password}/>
     </div>
   );
 }
